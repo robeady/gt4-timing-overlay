bf8c7b3a7809866c7ce17dc4817f4521c39e75bd
Rob Eady <robertj.eady@gmail.com>
2021-04-05 23:19:37 +0100

    logging in injected library

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,12 @@
 use game_data::GameData;
 use hudhook::{apply_hook, cleanup_hooks, RenderContext, RenderLoop};
+use log::{LevelFilter, Log, Metadata, Record};
 use ps2_types::Ps2Memory;
-use std::{ptr::null_mut, thread};
+use simplelog::{
+    ColorChoice, CombinedLogger, Config, ConfigBuilder, SharedLogger, TermLogger, TerminalMode,
+    ThreadLogMode, WriteLogger,
+};
+use std::{fs::File, io::Write, ptr::null_mut, thread};
 use winapi::um::{
     consoleapi::AllocConsole,
     winnt::{DLL_PROCESS_ATTACH, DLL_PROCESS_DETACH},
@@ -42,23 +47,76 @@ pub extern "stdcall" fn DllMain(
 ) {
     match reason {
         DLL_PROCESS_ATTACH => {
-            thread::spawn(|| {
-                unsafe {
-                    AllocConsole();
-                }
-                println!("Started thread, enabling hook...");
+            unsafe {
+                AllocConsole();
+            }
+            if let Err(e) = thread::Builder::new().spawn(|| {
+                let log_config = ConfigBuilder::new()
+                    .set_thread_level(LevelFilter::Error)
+                    .set_thread_mode(ThreadLogMode::Both)
+                    .build();
+                CombinedLogger::init(vec![
+                    TermLogger::new(
+                        LevelFilter::Debug,
+                        log_config.clone(),
+                        TerminalMode::Mixed,
+                        ColorChoice::Auto,
+                    ),
+                    Box::new(AlwaysFlush(WriteLogger::new(
+                        LevelFilter::Debug,
+                        log_config.clone(),
+                        File::create("gt4timing.log").unwrap(),
+                    ))),
+                ])
+                .unwrap_or_else(|e| println!("{}", e));
+
+                log::info!("Started thread, enabling hook");
                 match apply_hook(Box::new(Gt4TimingRenderLoop {
                     game_data: GameData::in_same_process(),
                 })) {
-                    Ok(_) => println!("Hook enabled"),
-                    Err(e) => println!("Hook errored: {:?}", e),
+                    Ok(_) => log::info!("Hook enabled"),
+                    Err(e) => log::error!("Hook errored: {:?}", e),
                 }
-            });
+            }) {
+                println!("Error spawning thread: {}", e)
+            }
         }
         DLL_PROCESS_DETACH if lp_reserved == null_mut() => {
             // lp_reserved == NULL => FreeLibrary called or DLL load failed, rather than process termination
-            cleanup_hooks().unwrap_or_else(|e| println!("{}", e));
+            cleanup_hooks().unwrap_or_else(|e| log::error!("{}", e));
         }
         _ => {}
+    }
+}
+
+struct AlwaysFlush(Box<dyn SharedLogger>);
+
+impl Log for AlwaysFlush {
+    fn enabled(&self, metadata: &Metadata<'_>) -> bool {
+        self.0.enabled(metadata)
+    }
+
+    fn log(&self, record: &Record<'_>) {
+        self.0.log(record);
+        // here's the important bit:
+        self.flush();
+    }
+
+    fn flush(&self) {
+        self.0.flush()
+    }
+}
+
+impl SharedLogger for AlwaysFlush {
+    fn level(&self) -> LevelFilter {
+        self.0.level()
+    }
+
+    fn config(&self) -> Option<&Config> {
+        self.0.config()
+    }
+
+    fn as_log(self: Box<Self>) -> Box<dyn Log> {
+        self.0.as_log()
     }
 }
