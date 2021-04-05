94576b215365146bb7490db58e91fb11a3b42d2d
Rob Eady <robertj.eady@gmail.com>
2021-04-05 16:08:40 +0100

    do injection elsewhere

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
 use game_data::GameData;
 use process_memory::{Architecture, Pid, ProcessHandleExt, TryIntoProcessHandle};
-use winapi::shared::minwindef::HMODULE;
 
 mod game_data;
-mod inject;
 mod processes;
 mod ps2_types;
 mod scan_memory;
@@ -15,21 +13,12 @@ pub struct Locations {
 }
 
 fn main() {
-    env_logger::init();
-
     let pid = processes::get_pcsx2_process_id();
     let handle = (pid as Pid)
         .try_into_process_handle()
         .unwrap()
         .set_arch(Architecture::Arch32Bit);
 
-    let dll = "target/i686-pc-windows-msvc/release/timing_lib.dll";
-
-    let hmodule = dbg!(inject::inject(pid as u32, dll).unwrap());
-
     let gd = GameData::connect(handle);
-    ui::render_window(gd, move || {
-        let free_library_ret_val = inject::uninject(pid as u32, hmodule).unwrap();
-        println!("{}", free_library_ret_val);
-    });
+    ui::render_window(gd, || {});
 }
