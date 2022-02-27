ea307b429212a8287f8d7cf013bd73dc6ca81cd6
Rob Eady <robertj.eady@gmail.com>
2022-02-27 16:14:32 +0000

    improve error if no pcsx2 process found

--- src/processes.rs
+++ src/processes.rs
@@ -3,5 +3,10 @@ use sysinfo::{ProcessExt, SystemExt};
 pub fn get_pcsx2_process_id() -> usize {
     let mut system = sysinfo::System::new_all();
     system.refresh_processes();
-    return system.get_process_by_name("pcsx2")[0].pid();
+    let candidates = system.get_process_by_name("pcsx2");
+    if candidates.len() == 0 {
+        panic!("no pcsx2 process found")
+    } else {
+        return candidates[0].pid();
+    }
 }
