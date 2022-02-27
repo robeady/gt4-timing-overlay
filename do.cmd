a94b0bd5d563b43d2bf9385410d39dc6861c1e31
Rob Eady <robertj.eady@gmail.com>
2022-02-27 16:13:12 +0000

    add run script and brief readme

---
+++ do.cmd
@@ -0,0 +1,14 @@
+call :%~1
+@ exit /b %errorlevel%
+
+:inject
+Injector.exe -n pcsx2.exe -i target\i686-pc-windows-msvc\release\timing_lib.dll
+@ exit /b %errorlevel%
+    
+:eject
+Injector.exe -n pcsx2.exe -e target\i686-pc-windows-msvc\release\timing_lib.dll
+@ exit /b %errorlevel%
+
+:build
+cargo build --release
+@ exit /b %errorlevel%
