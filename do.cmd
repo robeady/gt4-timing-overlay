fec5a6fa1b770d4e2e8831a8436e687ff992f167
Rob Eady <robertj.eady@gmail.com>
2022-03-04 23:24:23 +0000

    add run command

--- do.cmd
+++ do.cmd
@@ -4,11 +4,16 @@ call :%~1
 :inject
 Injector.exe -n pcsx2.exe -i target\i686-pc-windows-msvc\release\timing_lib.dll
 @ exit /b %errorlevel%
-    
+
 :eject
 Injector.exe -n pcsx2.exe -e target\i686-pc-windows-msvc\release\timing_lib.dll
 @ exit /b %errorlevel%
 
 :build
 cargo build --release
+@ exit /b %errorlevel%
+
+:run
+set RUST_BACKTRACE=1
+cargo run --release
 @ exit /b %errorlevel%
